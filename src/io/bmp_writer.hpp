4ac2a5213e2466b317c82291e3521440d74768ca
Max Franke <max@mumintroll.org>
2016-08-01 15:52:49 +0200

    Added support for BMP export
    Writes using some header-only BMP library.
    
    Signed-off-by: Max Franke <max@mumintroll.org>

---
+++ src/io/bmp_writer.hpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <memory>
+#include <string>
+#include "../third_party/bitmap/bitmap_image.hpp"
+
+#pragma once
+
+namespace io
+{
+    class BmpWriter
+    {
+        public:
+            BmpWriter(size_t const& widthHeight, std::shared_ptr<std::vector<double>> data)
+                : widthHeight(widthHeight), data(data) 
+            {}
+
+            /**
+             * Export to file.
+             * \param file name
+             */
+            void write(std::string const&) const;
+        protected:
+            BmpWriter() = delete;
+            BmpWriter(BmpWriter const&) = delete;
+            BmpWriter& operator= (BmpWriter const&) = delete;
+        private:
+            /**
+             * Width and height of image.
+             */
+            size_t const widthHeight;
+
+            /**
+             * Vector containing the data.
+             * Row-major.
+             * Assumed to be normalized.
+             */
+            std::shared_ptr<std::vector<double>> const data;
+    };
+}
