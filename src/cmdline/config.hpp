28d23c5761cb4862cabbb32ddf9dc9d6fc64126e
Max Franke <max@mumintroll.org>
2016-08-14 11:34:16 +0100

    Indented config.hpp
    Signed-off-by: Max Franke <max@mumintroll.org>

--- src/cmdline/config.hpp
+++ src/cmdline/config.hpp
@@ -11,30 +11,30 @@ namespace config
      * Map from strings to normalizer types, for command line parsing.
      */
     std::map<std::string, data::NormalizerType> const normalizers = {
-        { "lin", data::NormalizerType::LINEAR },
-        { "linear", data::NormalizerType::LINEAR },
-        { "log", data::NormalizerType::LOGARITHMIC_PLUS_ONE },
-        { "logarithmic", data::NormalizerType::LOGARITHMIC_PLUS_ONE }
+        { "lin"         , data::NormalizerType::LINEAR                  },
+        { "linear"      , data::NormalizerType::LINEAR                  },
+        { "log"         , data::NormalizerType::LOGARITHMIC_PLUS_ONE    },
+        { "logarithmic" , data::NormalizerType::LOGARITHMIC_PLUS_ONE    }
     };
 
     /**
      * Map from strings to colormaps, for command line parsing.
      */
     std::map<std::string, vis::PredefinedColormaps> const colormaps = {
-        { "redblue"     , vis::PredefinedColormaps::RED_BLUE        },
-        { "rdbu"        , vis::PredefinedColormaps::RED_BLUE        },
-
-        { "blackwhite"  , vis::PredefinedColormaps::GRAYSCALE       },
-        { "grey"        , vis::PredefinedColormaps::GRAYSCALE       },
-        { "gray"        , vis::PredefinedColormaps::GRAYSCALE       },
-        { "greyscale"   , vis::PredefinedColormaps::GRAYSCALE       },
-        { "grayscale"   , vis::PredefinedColormaps::GRAYSCALE       },
-
-        { "heat"        , vis::PredefinedColormaps::BLACK_BODY_HEAT },
-        { "blackbody"   , vis::PredefinedColormaps::BLACK_BODY_HEAT },
-        { "thermal"     , vis::PredefinedColormaps::BLACK_BODY_HEAT },
-
-        { "deepsea"     , vis::PredefinedColormaps::DEEP_SEA        },
-        { "ocean"       , vis::PredefinedColormaps::DEEP_SEA        }
+        { "redblue"     , vis::PredefinedColormaps::RED_BLUE            },
+        { "rdbu"        , vis::PredefinedColormaps::RED_BLUE            },
+                                                                        
+        { "blackwhite"  , vis::PredefinedColormaps::GRAYSCALE           },
+        { "grey"        , vis::PredefinedColormaps::GRAYSCALE           },
+        { "gray"        , vis::PredefinedColormaps::GRAYSCALE           },
+        { "greyscale"   , vis::PredefinedColormaps::GRAYSCALE           },
+        { "grayscale"   , vis::PredefinedColormaps::GRAYSCALE           },
+                                                                        
+        { "heat"        , vis::PredefinedColormaps::BLACK_BODY_HEAT     },
+        { "blackbody"   , vis::PredefinedColormaps::BLACK_BODY_HEAT     },
+        { "thermal"     , vis::PredefinedColormaps::BLACK_BODY_HEAT     },
+                                                                        
+        { "deepsea"     , vis::PredefinedColormaps::DEEP_SEA            },
+        { "ocean"       , vis::PredefinedColormaps::DEEP_SEA            }
     };
 }
