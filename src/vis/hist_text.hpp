41373e1aaad40d74b75e4e28aa933f59c2c119c6
Max Franke <max@mumintroll.org>
2016-08-18 14:30:00 +0100

    Added documentation to TextHistogram class
    Signed-off-by: Max Franke <max@mumintroll.org>

--- src/vis/hist_text.hpp
+++ src/vis/hist_text.hpp
@@ -9,17 +9,33 @@
 
 namespace vis
 {
+    /**
+     * Class for text histogram.
+     * Experimental.
+     */
     class TextHistogram
     {
         public:
+            /**
+             * Constructor.
+             *
+             * \param Shared pointer to a Histogram object.
+             */
             TextHistogram(std::shared_ptr<data::Histogram> const&);
 
+            /**
+             * Get strings representing lines of histogram.
+             * \return list of strings
+             */
             std::vector<std::string> getHistogram() const;
         protected:
             TextHistogram() = delete;
             TextHistogram(TextHistogram const&) = delete;
             TextHistogram& operator= (TextHistogram const&) = delete;
         private:
+            /**
+             * Histogram.
+             */
             std::shared_ptr<data::Histogram> histogram;
     };
 }
